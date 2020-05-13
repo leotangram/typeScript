ca0c78a7bf3f893b79071a65b3c544e2304a9193
Leonardo Omaña <leotangram@gmail.com>
2020-05-13 10:08:49 -0500

    Booleans

---
+++ booleans/main.js
@@ -0,0 +1,13 @@
+var esSuperman = true;
+var esBatman;
+var esAcuaman = true;
+if (esSuperman) {
+    console.log('Estamos salvados');
+}
+else {
+    console.log('Opps oh');
+}
+esSuperman = convertClark();
+function convertClark() {
+    return false;
+}
---
+++ booleans/main.ts
@@ -0,0 +1,15 @@
+let esSuperman: boolean = true
+let esBatman: boolean
+let esAcuaman: boolean = true
+
+if (esSuperman) {
+  console.log('Estamos salvados')
+} else {
+  console.log('Opps oh')
+}
+
+esSuperman = convertClark()
+
+function convertClark() {
+  return false
+}
