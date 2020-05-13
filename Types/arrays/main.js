c6d9d31af9fa4919e2630320149395faff94c3d9
Leonardo Omaña <leotangram@gmail.com>
2020-05-13 15:51:53 -0500

    Arrays

---
+++ Types/arrays/main.js
@@ -0,0 +1,3 @@
+var array = [1, 2, 3, 4, 5];
+var villians = ['Omega rojo', 'Dormammu', 'Duende verde'];
+console.log(villians[0].charAt(0));
---
+++ Types/arrays/main.ts
@@ -0,0 +1,4 @@
+let array: number[] = [1, 2, 3, 4, 5]
+let villians: string[] = ['Omega rojo', 'Dormammu', 'Duende verde']
+
+console.log(villians[0].charAt(0))
