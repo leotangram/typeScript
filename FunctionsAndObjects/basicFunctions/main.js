a61672c996e6d3676194291eee033ff89ca06937
Leonardo Omaña <leotangram@gmail.com>
2020-05-14 15:08:15 -0500

    Funciones básicas

---
+++ FunctionsAndObjects/basicFunctions/main.js
@@ -0,0 +1,9 @@
+var hero = 'Flash';
+function printHero() {
+    return hero;
+}
+var activeBatiSignal = function () {
+    return 'Batiseñal activada';
+};
+console.log(printHero());
+console.log(activeBatiSignal());
---
+++ FunctionsAndObjects/basicFunctions/main.ts
@@ -0,0 +1,12 @@
+let hero: string = 'Flash'
+
+function printHero(): string {
+  return hero
+}
+
+let activeBatiSignal = function (): string {
+  return 'Batiseñal activada'
+}
+
+console.log(printHero())
+console.log(activeBatiSignal())
