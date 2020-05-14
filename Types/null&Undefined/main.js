463c3dd3d76f90f0710037d95b3edc14599f2f2e
Leonardo Omaña <leotangram@gmail.com>
2020-05-14 10:24:42 -0500

    Null y undefined

---
+++ Types/null&Undefined/main.js
@@ -0,0 +1,10 @@
+var ironman;
+ironman = 'Tony';
+ironman = null;
+var number = undefined;
+number = 10;
+var object = {};
+object = { position: 'Oli' };
+console.log(object);
+object = null;
+console.log(object);
---
+++ Types/null&Undefined/main.ts
@@ -0,0 +1,17 @@
+let ironman: string | null
+ironman = 'Tony'
+
+ironman = null
+
+let number: number | undefined = undefined
+
+number = 10
+
+let object: object | null = {}
+
+object = { position: <string>'Oli' }
+
+console.log(object)
+
+object = null
+console.log(object)
