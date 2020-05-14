5f302673719af7a7344a9601c4834f103e89b22e
Leonardo Omaña <leotangram@gmail.com>
2020-05-14 15:35:45 -0500

    Parámetros obligatorios en las funciones

---
+++ FunctionsAndObjects/mandatoryFunctionParameters/main.js
@@ -0,0 +1,5 @@
+function completeName(name, surname) {
+    return name + " " + surname;
+}
+var name = completeName('clark', 'kent');
+console.log(name);
---
+++ FunctionsAndObjects/mandatoryFunctionParameters/main.ts
@@ -0,0 +1,7 @@
+function completeName(name: string, surname: string): string {
+  return `${name} ${surname}`
+}
+
+let name = completeName('clark', 'kent')
+
+console.log(name)
