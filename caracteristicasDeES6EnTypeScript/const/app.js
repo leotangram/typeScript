ae80746c2981918495558f401fb42c0b9bcb1bc9
Leonardo Omaña <leotangram@gmail.com>
2020-05-15 16:21:48 -0500

    Consts

---
+++ caracteristicasDeES6EnTypeScript/const/app.js
@@ -0,0 +1,17 @@
+"use strict";
+var OPTIONS = 'Activo';
+if (true) {
+    var OPTIONS_1 = 'Desactivado';
+}
+for (var _i = 0, _a = [1, 2, 3, 4, 5, 6]; _i < _a.length; _i++) {
+    var i = _a[_i];
+    console.log(i);
+}
+var OBJECT = {
+    state: true,
+    audio: 10,
+    last: 'main'
+};
+OBJECT.state = false;
+console.log(OBJECT);
+//# sourceMappingURL=app.js.map
---
+++ caracteristicasDeES6EnTypeScript/const/app.ts
@@ -0,0 +1,19 @@
+const OPTIONS: string = 'Activo'
+
+if (true) {
+  const OPTIONS: string = 'Desactivado'
+}
+
+for (const i of [1, 2, 3, 4, 5, 6]) {
+  console.log(i)
+}
+
+const OBJECT = {
+  state: true,
+  audio: 10,
+  last: 'main'
+}
+
+OBJECT.state = false
+
+console.log(OBJECT)
