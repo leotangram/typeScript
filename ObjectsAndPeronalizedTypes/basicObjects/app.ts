3ad660293e3c8fc212e22e1f1e50d04e793172b2
Leonardo Omaña <leotangram@gmail.com>
2020-05-15 09:38:31 -0500

    Basic Objects

---
+++ ObjectsAndPeronalizedTypes/basicObjects/app.ts
@@ -0,0 +1,11 @@
+let flash = {
+  name: 'Barry Allen',
+  age: 24,
+  powers: ['Puede correr muy rápido', 'Viajar en el tiempo']
+}
+
+flash = {
+  name: 'Clark Kent',
+  age: 500,
+  powers: ['Volar']
+}
