f9f57ee66e25b977b9fa50a65ed8e5c027f51599
Leonardo Omaña <leotangram@gmail.com>
2020-05-15 09:43:06 -0500

    Como crear objetos con tipos específicos

---
+++ ObjectsAndPeronalizedTypes/objectsWithSpecificTypes/app.js
@@ -0,0 +1,10 @@
+var flash = {
+    name: 'Barry Allen',
+    age: 24,
+    powers: ['Puede correr muy rápido', 'Viajar en el tiempo']
+};
+flash = {
+    name: 'Clark Kent',
+    age: 500,
+    powers: ['Volar']
+};
