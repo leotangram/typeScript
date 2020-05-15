890acb4f41b02ab42ab1f9db62e79b6706da2490
Leonardo Omaña <leotangram@gmail.com>
2020-05-15 10:38:20 -0500

    Revisar el tipo de un objeto o variable

---
+++ ObjectsAndPeronalizedTypes/reviewObjectOrVariableType/app.js
@@ -0,0 +1,10 @@
+var cosa = function () {
+    return null;
+};
+console.log(typeof cosa);
+if (typeof cosa === 'string') {
+    console.log('Cosa, es un string');
+}
+else {
+    console.log('Este codigo, indica que no es un string');
+}
---
+++ ObjectsAndPeronalizedTypes/reviewObjectOrVariableType/app.ts
@@ -0,0 +1,11 @@
+let cosa: any = function () {
+  return null
+}
+
+console.log(typeof cosa)
+
+if (typeof cosa === 'string') {
+  console.log('Cosa, es un string')
+} else {
+  console.log('Este codigo, indica que no es un string')
+}
