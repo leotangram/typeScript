76ffc0f3665ec072f0612efe195f9d16e69ad075
Leonardo Omaña <leotangram@gmail.com>
2020-05-18 11:16:02 -0500

    Definition

---
+++ Classes/00-definition/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var Avenger = (function () {
+    function Avenger() {
+        this.name = 'Antman';
+    }
+    return Avenger;
+}());
+var antman = new Avenger();
+console.log(antman);
+//# sourceMappingURL=app.js.map
---
+++ Classes/01-constructor/app.ts
@@ -0,0 +1,11 @@
+class Avenger {
+  name: string = 'Antman'
+  equipment: string
+  realName: string
+  canFight: boolean
+  fightWin: number
+}
+
+let antman: Avenger = new Avenger()
+
+console.log(antman)
