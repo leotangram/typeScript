f2e8753fa9944fc4cea82ccfc476ed35a79570dd
Leonardo Omaña <leotangram@gmail.com>
2020-05-18 09:16:37 -0500

    Destructuración de arreglos

---
+++ caracteristicasDeES6EnTypeScript/arrayDestructuring/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var avengers = ['Samuel Jackson', 'Robert Downey Jr', 'Paul Bettany'];
+var avenger3 = avengers[2];
+console.log(avenger3);
+//# sourceMappingURL=app.js.map
---
+++ caracteristicasDeES6EnTypeScript/arrayDestructuring/app.ts
@@ -0,0 +1,7 @@
+let avengers = ['Samuel Jackson', 'Robert Downey Jr', 'Paul Bettany']
+
+let [, , avenger3] = avengers
+
+// console.log(avenger1)
+// console.log(avenger2)
+console.log(avenger3)
